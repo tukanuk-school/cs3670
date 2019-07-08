a037ec5d1d5718f0f461d23c456bd6b1d872da1a
Tukanuk <benjamin@idavidson.ca>
2019-07-08 15:32:41 -0400

    added .gitignore

--- TCPclient.py
+++ TCPclient.py
@@ -1,13 +1,13 @@
 from socket import *
 
-serverName = '192.168.0.146'
+serverName = 'localhost'  # 64.90.34.17'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
 message = ""
 while message != "q":
-    message = input ('Input lowercase sentence: ')
+    message = input('Input lowercase sentence: ')
     clientSocket.send(message.encode())
 
     modifiedMessage = clientSocket.recv(1024)
